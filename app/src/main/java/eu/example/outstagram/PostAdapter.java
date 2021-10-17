4e5b4daef43e2df05bf30c1083998b90b11fccc4
U190709012 <borakotan@posta.mu.edu.tr>
2021-10-17 10:02:34 +0300

    Initial commit

--- app/src/main/java/eu/example/outstagram/PostAdapter.java
+++ app/src/main/java/eu/example/outstagram/PostAdapter.java
@@ -1,7 +1,7 @@
 package eu.example.outstagram;
 
+import java.util.List;
 import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,32 +10,33 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class PostAdapter extends BaseAdapter {
     List<Post> posts;
-    private LayoutInflater inflater;
-    public PostAdapter(Activity activity, List<Post> posts){
+    LayoutInflater inflater;
+
+    public PostAdapter(Activity activity, List<Post> posts) {
         this.posts = posts;
-        inflater = (LayoutInflater) activity.getSystemService(
-                Context.LAYOUT_INFLATER_SERVICE);
+        inflater = activity.getLayoutInflater();
     }
+
     @Override
     public int getCount() {
         return posts.size();
     }
+
     @Override
     public Object getItem(int position) {
         return posts.get(position);
     }
+
     @Override
     public long getItemId(int position) {
         return position;
     }
+
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
-        View rowView;
-        rowView = inflater.inflate(R.layout.row,null);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View rowView = inflater.inflate(R.layout.row, null);
         EditText txtMessage = (EditText) rowView.findViewById(R.id.txtMessage);
         TextView txtLocation = (TextView) rowView.findViewById(R.id.txtLocation);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.imageView);
